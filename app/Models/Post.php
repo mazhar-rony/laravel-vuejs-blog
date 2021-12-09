acf54df319120ecf5099d6d27a07c1b38d45a5f5
mazhar-rony <mazhar.rony@gmail.com>
2021-12-09 09:38:32 +0600

    database migration, model creation, relationship done

---
+++ app/Models/Post.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
+    
+}
